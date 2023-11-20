be1f4d053d1c1c77c9e7b9d280df3063187cbed9
OcaenEyes <1450136519@qq.com>
2023-11-20 22:02:52 +0800

    Update log.cpp

--- ocean_http/log/log.cpp
+++ ocean_http/log/log.cpp
@@ -1,3 +1,11 @@
+/*
+ * @Author: OCEAN.GZY
+ * @Date: 2023-11-20 21:09:16
+ * @LastEditors: OCEAN.GZY
+ * @LastEditTime: 2023-11-20 22:01:04
+ * @FilePath: /ocean_http/log/log.cpp
+ * @Description: 注释信息
+ */
 #include <string.h>
 #include <time.h>
 #include <sys/time.h>
@@ -7,33 +15,184 @@
 
 log::log()
 {
+    m_count = 0;
+    m_is_async = false;
 }
 
 log::~log()
 {
+    if (m_fp != NULL)
+    {
+        fclose(m_fp);
+    }
 }
 
 void *log::async_write_log()
 {
+    std::string single_log;
+
+    // 从阻塞队列中 取出一个日志string，写入文件
+    while (m_log_queue->pop(single_log))
+    {
+        m_mutex.lock();
+        fputs(single_log.c_str(), m_fp);
+        m_mutex.unlock();
+    }
 }
 
-log* log::get_instance()
+log *log::get_instance()
 {
-    
+    static log instance;
+    return &instance;
 }
 
-void* log::flusg_log_thread(void *args)
+void *log::flusg_log_thread(void *args)
 {
-    
+    log::get_instance()->async_write_log();
 }
 
 bool log::init(const char *file_name, int close_log, int log_buf_size, int split_lines, int max_queue_size)
 {
-    
+    // 如果设置了max_queue_sizem 则设置为异步
+    if (max_queue_size >= 1)
+    {
+        m_is_async = true;
+        m_log_queue = new block_queue<std::string>(max_queue_size);
+        pthread_t tid;
+
+        // flush_log_thread为回调函数，表示创建线程 异步写日志
+        pthread_create(&tid, NULL, flusg_log_thread, NULL);
+    }
+
+    m_close_log = close_log;
+    m_log_buf_size = log_buf_size;
+    m_buf = new char[m_log_buf_size];
+    memset(m_buf, '\0', m_log_buf_size);
+    m_split_lines = split_lines;
+
+    time_t t = time(NULL);
+    struct tm *sys_tm = localtime(&t);
+    struct tm my_tm = *sys_tm;
+
+    const char *p = strrchr(file_name, '/');
+    char log_full_name[256] = {0};
+
+    if (p == NULL)
+    {
+        snprintf(log_full_name, 255, "%d_%02d_%02d_%s", my_tm.tm_year + 1900, my_tm.tm_mon + 1, my_tm.tm_mday, file_name);
+    }
+    else
+    {
+        strcpy(log_name, p + 1);
+        strncpy(dir_name, file_name, p - file_name + 1);
+        snprintf(log_full_name, 255, "%s%d_%02d_%02d_%s", dir_name, my_tm.tm_year + 1900, my_tm.tm_mon + 1, my_tm.tm_mday, file_name);
+    }
+
+    m_today = my_tm.tm_mday;
+
+    m_fp = fopen(log_full_name, "a");
+    if (m_fp)
+    {
+        return false;
+    }
+    return true;
 }
 
 void log::write_log(int level, const char *format, ...)
 {
-    
+    struct timeval now = {0, 0};
+    gettimeofday(&now, NULL);
+
+    time_t t = now.tv_sec;
+    struct tm *sys_tm = localtime(&t);
+    struct tm my_tm = *sys_tm;
+    char s[16] = {0};
+    switch (level)
+    {
+    case 0:
+        strcpy(s, "[debug]:");
+        break;
+    case 1:
+        strcpy(s, "[info]:");
+        break;
+    case 2:
+        strcpy(s, "[warn]:");
+        break;
+    case 3:
+        strcpy(s, "[error]:");
+        break;
+    default:
+        strcpy(s, "[info]:");
+        break;
+    }
+
+    // 写入一个log, 对m_count++,m_split_lines最大行数
+    m_mutex.lock();
+    m_count++;
+
+    // 每天日志
+    if (m_today != my_tm.tm_mday || m_count % m_split_lines == 0)
+    {
+        char new_log[256] = {0};
+        fflush(m_fp);
+        fclose(m_fp);
+        char tail[16] = {0};
+
+        snprintf(tail, 16, "%d_%02d_%02d_", my_tm.tm_year + 1900, my_tm.tm_mon + 1, my_tm.tm_mday);
+        if (m_today != my_tm.tm_mday)
+        {
+            snprintf(new_log, 255, "%s%s%s", dir_name, tail, log_name);
+            m_today = my_tm.tm_mday;
+            m_count = 0;
+        }
+        else
+        {
+            snprintf(new_log, 255, "%s%s%s.%lld", dir_name, tail, log_name, m_count / m_split_lines);
+        }
+        m_fp = fopen(new_log, "a");
+    }
+
+    m_mutex.unlock();
+
+    va_list valist;
+    va_start(valist, format);
+
+    std::string log_str;
+    m_mutex.lock();
+
+    // 写入的具体时间内容格式
+    int n = snprintf(
+        m_buf, 48, "%d-%02d-%02d %02d:%02d:%02d.%06ld %s ",
+        my_tm.tm_year + 1900, my_tm.tm_mon + 1, my_tm.tm_mday,
+        my_tm.tm_hour, my_tm.tm_min, my_tm.tm_sec, now.tv_usec, s);
+
+    int m = vsnprintf(m_buf + n, m_log_buf_size - n - 1, format, valist);
+
+    m_buf[n + m] = '\n';
+    m_buf[n + m + 1] = '\0';
+    log_str = m_buf;
+
+    m_mutex.unlock();
+
+    if (m_is_async && !m_log_queue->full())
+    {
+        m_log_queue->push(log_str);
+    }
+    else
+    {
+        m_mutex.lock();
+        fputs(log_str.c_str(), m_fp);
+        m_mutex.unlock();
+    }
+
+    va_end(valist);
 }
 
+void log::flush(void)
+{
+    m_mutex.lock();
+
+    // 强制刷新写入 流缓冲区
+    fflush(m_fp);
+    m_mutex.unlock();
+}
